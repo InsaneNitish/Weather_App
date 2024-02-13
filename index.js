748f4bc2df9dde2202f74c4c38bf1e90a2a61d25
Insanenitish <nitishtthakur786@gmail.com>
2024-02-13 19:30:03 +0530

    1. Done only front-end.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 //importing importent modules from node-module.
-import 
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+//creating app 
+const app = express();
+const port = 3000;
+
+//connecting static files
+app.use(express.static("public"));
+
+//rendering the homepage.
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
+
+
+//listening to the express server.
+app.listen(port,()=>{
+    console.log(`Server starting at port ${port}`);
+})
