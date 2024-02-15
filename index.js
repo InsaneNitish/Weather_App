eebf6ed122be689d7dc4d7fc7f95ee0878b639fd
Insanenitish <nitishtthakur786@gmail.com>
2024-02-16 02:29:57 +0530

    1. Done Simple Backend work in this commit. 2. Added the weather API and the location API. 3. Added the weather icons. 4. Added the weather description. 5. Added the temperature. 6. Added the location.

--- index.js
+++ index.js
@@ -6,9 +6,44 @@ import axios from "axios";
 //creating app 
 const app = express();
 const port = 3000;
+const API_URL_1 = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const API_URL_2 = "?unitGroup=metric&key=LAEJN83PBSRSUAHPPMJ7X2S89&contentType=json";
+
 
 //connecting static files
 app.use(express.static("public"));
+
+//using api to get the data from the server.
+app.use(bodyParser.urlencoded({extended:true}));
+
+// const x = ("#loc");
+
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else { 
+//     x.innerHTML = "Geolocation is not supported by this browser.";
+//   }
+// }
+// var lat;
+// var long;
+// function showPosition(position) {
+//   x.innerHTML = "Latitude: " + position.coords.latitude + 
+//   "<br>Longitude: " + position.coords.longitude;
+//   lat = position.coords.latitude;
+//   long = position.coords.longitude;
+// }
+
+app.post("/",async (req,res)=>{
+    const city = req.body.city;
+    console.log(req.body);
+    const result = await axios.get(API_URL_1 + city +  API_URL_2);
+    res.locals.weather = result.data;
+    console.log(result.data);
+    res.render("index.ejs");
+
+});
+
 
 //rendering the homepage.
 app.get("/",(req,res)=>{
